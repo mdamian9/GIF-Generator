9b9dca3114733b1d3bc118e6ccfd3b1b8dbc5329
md_dev <m.damian9@hotmail.com>
2018-06-24 03:01:17 -0700

    Progress on javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     var myArray = ["dog", "cat", "turtle", "eagle", "zebra"];
 
     for (var i = 0; i < myArray.length; i++) {
-        $("#animal-btns").append('<button class="animal-btn" animal-data="' + myArray[i] + '">' + myArray[i] + '</button>');
+        $("#animal-btns").append('<button class="animal-btn" data-animal="' + myArray[i] + '">' + myArray[i] + '</button>');
     };
 
     $(".animal-btn").on("click", function () {
-        var animal = $(this).attr("animal-data");
+        var animal = $(this).attr("data-animal");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             animal + "&api_key=dc6zaTOxFJmzC&limit=10";
         $.ajax({
@@ -14,7 +14,7 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-            console.log(response);    
+            console.log(response);
 
             var results = response.data;
 
@@ -23,8 +23,9 @@ $(document).ready(function () {
                 var newDiv = $("<div>");
                 var p = $("<p>");
                 p.text("Rating: " + results[i].rating);
-                var newImage = $("<img>");
-                newImage.attr("src", results[i].images.fixed_height.url);
+                var newImage = $("<img class='gif-img' data-animate='" + results[i].images.fixed_height.url + "' data-still='" +
+                    results[i].images.fixed_height_still.url + "' data-state='still'>");
+                newImage.attr("src", results[i].images.fixed_height_still.url);
                 newDiv.append(p);
                 newDiv.append(newImage);
                 $("#animal-gifs").prepend(newDiv);
@@ -34,6 +35,26 @@ $(document).ready(function () {
         });
     });
 
+    // this function is not working properly yet
+    $(".gif-img").on("click", function () {
+
+        alert("click on gif works");
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            state = $(this).attr("data-state", "animate");
+        } else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            state = $(this).attr("data-state", "still");
+        };
+
+        console.log(state);
+
+    });
+
+    // Prevents page from refreshing when clicking on "Submit"
     $("#animal-form").submit(function (e) {
         e.preventDefault();
 
@@ -47,3 +68,11 @@ $(document).ready(function () {
 
 
 });
+
+// Fix start / stop gif by clicking on gif
+// Obtain user input
+// Save user input to variable
+// Create new button based on user input
+// Give new button same functionality as the rest
+
+// When button is clicked, CSS is changed (?)
