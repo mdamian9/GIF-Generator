9ba1532dee79ffa70cea27067e8d204fd2f51192
md_dev <m.damian9@hotmail.com>
2018-06-23 20:30:28 -0700

    Created buttons that will generate GIFs, input form is in place

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     var myArray = ["dog", "cat", "turtle", "eagle", "zebra"];
+
+    for (var i = 0; i < myArray.length; i++) {
+        $("#animal-btns").append('<button id="animal-btn" animal-name="' + myArray[i] + '">' + myArray[i]+ '</button>');
+    }
+
 });
