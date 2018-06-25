4d15c0e05915630abba759dfb382ac135101005c
md_dev <m.damian9@hotmail.com>
2018-06-25 01:20:50 -0700

    Added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,40 @@
 $(document).ready(function () {
 
+    // Create array of animals.
     var myArray = ["dog", "cat", "turtle", "eagle", "zebra"];
 
+    // For every animal in array, create and append a new button element with the following attributes:
     for (var i = 0; i < myArray.length; i++) {
         $("#animal-btns").append('<button class="animal-btn" data-animal="' + myArray[i] + '">' + myArray[i] + '</button>');
     };
 
-    // Prevents page from refreshing when clicking on "Submit"
+    // When user clicks on submit button in the form #animal-form, execute the following function:
     $("#animal-form").submit(function (e) {
 
+        // Prevents page from refreshing when clicking on "Submit".
         e.preventDefault();
 
+        // Take value of user input and store it in var animalName.
         var animalName = $("#animal-input").val();
 
+        // If user clicked submit without any input, alert user of error. If there is valid input, then create and append a new 
+        // animal button with the same attributes as the originals.
         if ($("#animal-input").val() === "") {
             alert("There was no input. Please try again!");
         } else {
             $("#animal-btns").append('<button class="animal-btn" data-animal="' + animalName + '">' + animalName + '</button>');
         };
 
-        console.log($("#animal-input").val());
-
     });
 
+    // When user clicks on an element with class=".animal-btn" inside an element with id="animal-btns" - execute the following function:
     $("#animal-btns").on("click", ".animal-btn", function () {
 
+        // Clear any content in #animal-gifs, where gifs will be displayed. This is so that gifs aren't appended forever.
         $("#animal-gifs").html("");
+
+        // Store the name of the animal (value of "data-animal") in new var animal. Store query URL in new var queryURL, make ajax
+        // request, then once response is obtained execute the following function, passing response as a parameter.
         var animal = $(this).attr("data-animal");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             animal + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -34,10 +43,11 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
 
-            console.log(response);
-
+            // Take response.data (array of gifs data) and store it into results
             var results = response.data;
 
+            // For every gif in array: create new div element, and new p element. Display rating in p element. Create and append new image 
+            // tag with data from ajax request. Gif will be still in the beginning. 
             for (var i = 0; i < results.length; i++) {
 
                 var newDiv = $("<div>");
@@ -56,8 +66,10 @@ $(document).ready(function () {
 
     });
 
-    $("#reset-btns").on("click", function() {
+    // If user clicks "Reset Buttons" button, clear added buttons and go back to original ones from array
+    $("#reset-btns").on("click", function () {
 
+        // Clear #animal-btns content, append original buttons
         $("#animal-btns").html("");
         for (var i = 0; i < myArray.length; i++) {
             $("#animal-btns").append('<button class="animal-btn" data-animal="' + myArray[i] + '">' + myArray[i] + '</button>');
@@ -65,12 +77,13 @@ $(document).ready(function () {
 
     });
 
-    // Function: when user clicks on an element with class=".gif-img" inside an element with id="animal-gifs" - if image is still then
-    // make gif animate. If gif is animating, make gif still
+    // When user clicks on an element with class=".gif-img" inside an element with id="animal-gifs" - execute function
     $("#animal-gifs").on("click", ".gif-img", function () {
 
+        // Store gif data-state value into var state
         var state = $(this).attr("data-state");
 
+        // If image is still then make gif animate. If gif is animating, make gif still
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             state = $(this).attr("data-state", "animate");
